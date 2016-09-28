53279d94c0629a499ec87a219efa86cc6053fd3b
Ingwie <Ingwie@users.noreply.github.com>
2016-09-28 02:11:15 +0200

    Add new files

---
+++ NextStepRc/targets/stock/lcd_simu_driver.h
@@ -0,0 +1,18 @@
+/*
+*************************************************************
+*                      NEXTSTEPRC                           *
+*                                                           *
+*             -> Build your DIY MEGA 2560 TX                *
+*                                                           *
+*      Based on code named                                  *
+*      OpenTx - https://github.com/opentx/opentx            *
+*                                                           *
+*         Only avr code here for lisibility ;-)             *
+*                                                           *
+*  License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html  *
+*                                                           *
+*************************************************************
+*/
+
+//#include "../../nextsteprc.h"
+#include "../../../NextStepRc_Simulator/NextStepRc_SimulatorMain.h"
