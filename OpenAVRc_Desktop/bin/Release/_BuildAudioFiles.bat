41b25e9f9b8f488cdee2e01c948430001c13da71
Ingwie <theingwie@gmail.com>
2016-12-13 14:44:27 +0100

    Revert "Revert "Builauduio fonctionne !""
    This reverts commit c92e24081aff814da9d7ee1c3100e35e6a0e01ee.

---
+++ OpenAVRc_Desktop/bin/Release/_BuildAudioFiles.bat
@@ -0,0 +1,42 @@
+REM Clean Directory
+
+rmdir /S /Q VOICEMP3
+rmdir /S /Q VOICEAD4
+
+REM Normalise and cut silence
+
+for /r %%a in ("*.wav") do CALL tools\sox --norm=-1 %%a %%~na.aif silence 1 0.1 0.1% reverse highpass 300
+REM Cut silence
+for /r %%a in ("*.aif") do CALL tools\sox %%a %%~na.wav silence 1 0.1 1% reverse pad 0.0 0.1 
+del *.aif /s
+
+REM Convert to mp3 (JQ6500)
+
+for %%a in ("*.wav") do CALL tools\lame -q0 --abr 256 %%a %%~na.mp3
+md VOICEMP3
+move /y *.mp3 VOICEMP3
+
+REM Convert to ADPCM 4 bits (WTV20)
+
+for %%a in ("*.wav") do CALL tools\AD4CONVERTER -E4 %%a
+md VOICEAD4
+move /y *.AD4 VOICEAD4
+
+REM Clean
+
+del *.wav /s
+
+REM Splash screen finished !
+
+echo off
+cls
+echo ********************************************
+echo ********************************************
+echo                 OpenAVRc
+echo ********************************************
+echo ********************************************
+echo LES FICHIERS SONT DANS LES REPERTOIRES VOICE
+echo ********************************************
+echo ********************************************
+pause
+:end
