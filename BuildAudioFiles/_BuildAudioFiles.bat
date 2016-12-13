dc70dd68efb8a8a2b1b6ef446d5a3ec73b658d81
Ingwie <theingwie@gmail.com>
2016-12-13 14:42:30 +0100

    Un peut de ménage

--- BuildAudioFiles/_BuildAudioFiles.bat
+++ BuildAudioFiles/_BuildAudioFiles.bat
@@ -1,29 +1,24 @@
-
-rmdir /S /Q VOICEMP3
-rmdir /S /Q VOICEAD4
-
+start "NEXTSTEPRC" /wait OpenTXspeaker.exe
 REM Normalise and cut silence
-for /r %%a in ("*.wav") do CALL C:\OpenAVRc\BuildAudioFiles\SOUNDS\tools\sox --norm=-1 %%a %%~na.aif silence 1 0.1 0.1% reverse highpass 300
+for /r %%a in ("*.wav") do CALL SOUNDS\tools\sox --norm=-1 %%a %%~na.aif silence 1 0.1 0.1% reverse highpass 300
 REM Cut silence
-REM pause
-for /r %%a in ("*.aif") do CALL C:\OpenAVRc\BuildAudioFiles\SOUNDS\tools\sox %%a %%~na.wav silence 1 0.1 1% reverse pad 0.0 0.1 
+for /r %%a in ("*.aif") do CALL SOUNDS\tools\sox %%a %%~na.wav silence 1 0.1 1% reverse pad 0.0 0.1 
 del *.aif /s
-REM pause
 REM Convert to mp3
-for %%a in ("*.wav") do CALL C:\OpenAVRc\BuildAudioFiles\SOUNDS\tools\lame -q0 --abr 256 %%a %%~na.mp3
+for %%a in ("*.wav") do CALL SOUNDS\tools\lame -q0 --abr 256 %%a %%~na.mp3
 md VOICEMP3
 move /y *.mp3 VOICEMP3
 rem del *.mp3 /s
 REM Convert to ADPCM 4 bits
-REM REM for %%a in ("*.wav") do CALL SOUNDS\tools\AD4CONVERTER -E4 %%a
-REM REM md VOICEAD4
-REM REM move /y *.AD4 VOICEAD4
+for %%a in ("*.wav") do CALL SOUNDS\tools\AD4CONVERTER -E4 %%a
+md VOICEAD4
+move /y *.AD4 VOICEAD4
 del *.wav /s
 echo off
 cls
 echo ********************************************
 echo ********************************************
-echo                 OpenAVRc
+echo                 NEXSTEPRC
 echo ********************************************
 echo ********************************************
 echo LES FICHIERS SONT DANS LES REPERTOIRES VOICE
