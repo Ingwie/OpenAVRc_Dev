3548a192f53cc1b81d1ee7a05203b9d1b3de150b
Ingwie <theingwie@gmail.com>
2016-12-22 15:08:09 +0100

    Nouveau header pour les applications PC

--- OpenAVRc_Desktop/OpenAVRc_DesktopMain.h
+++ OpenAVRc_Desktop/OpenAVRc_DesktopMain.h
@@ -1,11 +1,28 @@
-/***************************************************************
- * Name:      OpenAVRc_DesktopMain.h
- * Purpose:   Defines Application Frame
- * Author:    OpenAVRc_TEAM ()
- * Created:   2016-09-08
- * Copyright: OpenAVRc_TEAM ()
- * License:
- **************************************************************/
+ /*
+ **************************************************************************
+ *                                                                        *
+ *              This file is part of the OpenAVRc project.                *
+ *                                                                        *
+ *                         Based on code named                            *
+ *             OpenTx - https://github.com/opentx/opentx                  *
+ *                                                                        *
+ *                Only AVR code here for lisibility ;-)                   *
+ *                                                                        *
+ *   OpenAVRc is free software: you can redistribute it and/or modify     *
+ *   it under the terms of the GNU General Public License as published by *
+ *   the Free Software Foundation, either version 2 of the License, or    *
+ *   (at your option) any later version.                                  *
+ *                                                                        *
+ *   OpenAVRc is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *   GNU General Public License for more details.                         *
+ *                                                                        *
+ *       License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html          *
+ *                                                                        *
+ **************************************************************************
+*/
+
 
 #ifndef OpenAVRc_DESKTOPMAIN_H
 #define OpenAVRc_DESKTOPMAIN_H
