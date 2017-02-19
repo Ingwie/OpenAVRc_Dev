a368999ddefd0a843aa8c749fa2e32133916d485
jmentero <mentero@gmail.com>
2017-02-19 17:43:34 +0100

    Minimalistic first version of gvarsFrame.

--- OpenAVRc_Simulator/GvarsFrame.h
+++ OpenAVRc_Simulator/GvarsFrame.h
@@ -2,14 +2,10 @@
 #define GVARS_H
 
 //(*Headers(GvarsFrame)
-#include <wx/stattext.h>
-#include <wx/textctrl.h>
-#include <wx/statbox.h>
-#include <wx/slider.h>
 #include <wx/panel.h>
 #include <wx/grid.h>
 #include <wx/frame.h>
-#include <wx/gauge.h>
+#include <wx/timer.h>
 //*)
 
 class GvarsFrame: public wxFrame
@@ -20,103 +16,20 @@ class GvarsFrame: public wxFrame
 		virtual ~GvarsFrame();
 
     GvarsFrame *GvFr;
+    void PopulateGvarsFrame();
 
 		//(*Declarations(GvarsFrame)
-		wxSlider* Slider1;
-		wxPanel* Panel20;
-		wxPanel* Panel5;
-		wxGauge* Gauge3;
-		wxSlider* Slider2;
-		wxStaticText* StaticText2;
-		wxPanel* Panel4;
-		wxSlider* Slider5;
-		wxStaticText* StaticText6;
-		wxGauge* Gauge4;
-		wxGauge* Gauge1;
-		wxPanel* Panel21;
-		wxPanel* Panel11;
-		wxSlider* Slider3;
-		wxPanel* Panel9;
-		wxStaticText* StaticText8;
-		wxPanel* Panel8;
-		wxGauge* Gauge5;
+		wxGrid* GVARSGrid;
 		wxPanel* Panel1;
-		wxStaticText* StaticText1;
-		wxStaticText* StaticText3;
-		wxStaticBox* StaticBox1;
-		wxGrid* Grid1;
-		wxPanel* Panel6;
-		wxPanel* Panel3;
-		wxStaticBox* StaticBox2;
-		wxPanel* Panel16;
-		wxStaticText* StaticText5;
-		wxStaticText* StaticText7;
-		wxPanel* Panel7;
-		wxSlider* Slider4;
-		wxPanel* Panel18;
-		wxPanel* Panel15;
-		wxStaticBox* StaticBox3;
-		wxTextCtrl* TextCtrl1;
-		wxGauge* Gauge2;
-		wxPanel* Panel14;
-		wxPanel* Panel2;
-		wxPanel* Panel10;
-		wxStaticText* StaticText4;
-		wxPanel* Panel12;
-		wxPanel* Panel17;
-		wxStaticBox* StaticBox4;
-		wxPanel* Panel13;
-		wxPanel* Panel19;
+		wxTimer gvarsTimer;
 		//*)
 
 	protected:
 
 		//(*Identifiers(GvarsFrame)
-		static const long ID_STATICBOX3;
-		static const long ID_STATICBOX2;
 		static const long ID_GRID1;
-		static const long ID_STATICTEXT4;
-		static const long ID_STATICBOX1;
-		static const long ID_PANEL8;
-		static const long ID_PANEL21;
-		static const long ID_PANEL20;
-		static const long ID_PANEL19;
-		static const long ID_PANEL18;
-		static const long ID_PANEL17;
-		static const long ID_PANEL16;
-		static const long ID_PANEL15;
-		static const long ID_PANEL14;
-		static const long ID_PANEL13;
-		static const long ID_PANEL12;
-		static const long ID_PANEL11;
-		static const long ID_PANEL10;
-		static const long ID_PANEL9;
-		static const long ID_PANEL2;
-		static const long ID_PANEL4;
-		static const long ID_PANEL7;
-		static const long ID_PANEL6;
-		static const long ID_PANEL5;
-		static const long ID_PANEL3;
-		static const long ID_SLIDER2;
-		static const long ID_SLIDER5;
-		static const long ID_SLIDER4;
-		static const long ID_SLIDER3;
-		static const long ID_SLIDER1;
-		static const long ID_GAUGE2;
-		static const long ID_GAUGE5;
-		static const long ID_GAUGE4;
-		static const long ID_GAUGE3;
-		static const long ID_GAUGE1;
-		static const long ID_STATICTEXT2;
-		static const long ID_STATICTEXT5;
-		static const long ID_STATICTEXT3;
-		static const long ID_STATICTEXT1;
-		static const long ID_STATICBOX4;
-		static const long ID_TEXTCTRL1;
-		static const long ID_STATICTEXT7;
-		static const long ID_STATICTEXT8;
-		static const long ID_STATICTEXT6;
 		static const long ID_PANEL1;
+		static const long ID_TIMERGVARS;
 		//*)
 
 	private:
@@ -124,6 +37,9 @@ class GvarsFrame: public wxFrame
 		//(*Handlers(GvarsFrame)
 		void OnPanel4Paint(wxPaintEvent& event);
 		void OnClose(wxCloseEvent& event);
+		void OnGrid1CellLeftClick(wxGridEvent& event);
+		void OnGrid1CellLeftClick1(wxGridEvent& event);
+		void OngvarsTimerTrigger(wxTimerEvent& event);
 		//*)
 
 		DECLARE_EVENT_TABLE()
