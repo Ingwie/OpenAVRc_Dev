a9f7e7e684a54bf964b19e5a62682a747cd7fa36
jmentero <mentero@gmail.com>
2017-04-04 21:02:31 +0200

    Model name edit. Now working, thanks Bracame.

--- OpenAVRc_Simulator/ModelNameDialog.cpp
+++ OpenAVRc_Simulator/ModelNameDialog.cpp
@@ -1,6 +1,7 @@
 #include "ModelNameDialog.h"
 #include "OpenAVRc_SimulatorMain.h"
 #include <wx/msgdlg.h>
+#include <wx/valtext.h>
 
 //(*InternalHeaders(ModelNameDialog)
 #include <wx/intl.h>
@@ -20,13 +21,21 @@ END_EVENT_TABLE()
 
 ModelNameDialog::ModelNameDialog(wxWindow* parent,wxWindowID id,const wxPoint& pos,const wxSize& size)
 {
+  wxTextValidator validator(wxFILTER_INCLUDE_CHAR_LIST);
+  wxArrayString list;
+  wxString valid_chars(wxT(" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_-+:*=?"));
+  size_t len = valid_chars.Length();
+  for (size_t i=0; i<len; i++)
+  list.Add(wxString(valid_chars.GetChar(i)));
+  validator.SetIncludes(list);
+
 	//(*Initialize(ModelNameDialog)
 	Create(parent, id, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, _T("id"));
 	SetClientSize(wxSize(181,67));
 	Move(wxDefaultPosition);
 	Panel1 = new wxPanel(this, ID_PANEL1, wxPoint(0,0), wxSize(288,176), wxTAB_TRAVERSAL, _T("ID_PANEL1"));
 	StaticBox1 = new wxStaticBox(Panel1, ID_STATICBOX1, _("Nom du modèle"), wxPoint(0,0), wxSize(176,64), 0, _T("ID_STATICBOX1"));
-	TextCtrlnewName = new wxTextCtrl(Panel1, ID_TEXTCTRLNEWNAME, _("10 Chars Max"), wxPoint(40,24), wxDefaultSize, wxWANTS_CHARS, wxDefaultValidator, _T("ID_TEXTCTRLNEWNAME"));
+	TextCtrlnewName = new wxTextCtrl(Panel1, ID_TEXTCTRLNEWNAME, _("10 Chars Max"), wxPoint(40,24), wxDefaultSize, wxWANTS_CHARS, validator, _T("ID_TEXTCTRLNEWNAME"));
 	TextCtrlnewName->SetMaxLength(10);
 
 	Connect(ID_TEXTCTRLNEWNAME,wxEVT_COMMAND_TEXT_ENTER,(wxObjectEventFunction)&ModelNameDialog::OnTextCtrlnewNameTextEnter);
@@ -45,14 +54,15 @@ ModelNameDialog::~ModelNameDialog()
 	//*)
 }
 
-void ModelNameDialog::CheckEntryValues(wxString inString)
+
+/*void ModelNameDialog::CheckEntryValues(wxString inString)
 {
   std::string  x(inString);
   if (x.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_-+:*=? ") != std::string::npos){
       wxMessageBox(x +_(" :\nIl y a des caractères spéciaux non autorisés.\nVeuillez le changer.\nCaractères spéciaux autorisés : _-+:*=?"));
   }
 }
-
+*/
 
 void ModelNameDialog::OnInit(wxInitDialogEvent& event)
 {
@@ -64,7 +74,7 @@ void ModelNameDialog::OnTextCtrlnewNameTextEnter(wxCommandEvent& event)
 {
   wxString newName;
   newName = TextCtrlnewName->GetValue();
-  CheckEntryValues(newName);
+  //CheckEntryValues(newName);
   ConvWxstrToCharFw(newName, g_model.header.name, LEN_MODEL_NAME);
   Close();
 }
