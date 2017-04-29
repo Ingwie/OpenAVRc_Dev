8f3838fa9cea8069231f25ef1d864ae13b4ba416
sloped-soarer <eclrpk@leeds.ac.uk>
2017-04-29 18:13:00 +0100

    Change SPI mode for CYRF6936.

--- OpenAVRc/targets/evo_v1/evo_mega2560.cpp
+++ OpenAVRc/targets/evo_v1/evo_mega2560.cpp
@@ -126,9 +126,9 @@ inline void boardInit()
   RF_CS_CC2500_INACTIVE();
 
   UBRR3 = 0; // Reset is part of initialisation sequence.
-  UCSR3C = 0xC3; // UMSEL21:0 = 3 DORD2=0 CPHA2=1 CPOL2=1  USART in Master SPI mode, MSB first, Clock phase=1 Polarity=1.
+  UCSR3C = 0xC0; // UMSEL21:0 = 3 DORD2=0 CPHA2=0 CPOL2=0  USART in Master SPI mode, MSB first, Clock phase=0 Polarity=0.
   UCSR3B = (1 << RXEN3) | (1 << TXEN3); // Transmit and Receive.
-  UBRR3 = 1; // 4 MHz clock ... 16MHz/(2*(UBRR+1))
+  UBRR3 = 2; // 2.666 MHz clock ... 16MHz/(2*(UBRR+1))
 
   DDRJ |= (1<<PORTJ1) | (1<<PORTJ2); // | (1<<PORTJ3);
   DDRJ &= ~(1<<PORTJ0);
