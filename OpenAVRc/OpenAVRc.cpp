e8dd9159b5093cef95bcb2902bc22ce2c848e815
Ingwie <theingwie@gmail.com>
2018-01-15 21:25:35 +0100

    Restore PPMsim max channel

--- OpenAVRc/OpenAVRc.cpp
+++ OpenAVRc/OpenAVRc.cpp
@@ -79,7 +79,7 @@ void sendOptionsSettingsPpm()
                       STR_DUMMY,
                       STR_DUMMY,
                       STR_DUMMY);
-  if (s_current_protocol!=(PROTOCOL_PPM-1)) g_model.PPMNCH = limit<uint8_t>(0,g_model.PPMNCH,2);
+  if (s_current_protocol==(PROTOCOL_PPM16-1)) g_model.PPMNCH = limit<uint8_t>(0,g_model.PPMNCH,2);
   g_model.PPMFRAMELENGTH = (g_model.PPMNCH-2) * 8;
   RFModule.mode = NORMAL_MODE;
 }
