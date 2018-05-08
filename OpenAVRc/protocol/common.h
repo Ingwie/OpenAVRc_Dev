47cd028c408ebbfb156935b1a0d772d697fdde5a
Bracame <Ingwie@users.noreply.github.com>
2018-05-08 19:27:41 +0200

    Protocol/common.h : Remove unused defs.

--- OpenAVRc/protocol/common.h
+++ OpenAVRc/protocol/common.h
@@ -38,42 +38,13 @@
 
 #include "../OpenAVRc.h"
 
-//uint8_t sub_protocol;
-//uint8_t protocol;
-//uint8_t option;
-//uint8_t cur_protocol[3];
-//uint8_t prev_option;
-//uint8_t prev_power=0xFD; // unused power value
-//uint8_t  hopping_frequency[50];
-
-
-//uint8_t read_bind_sw();
-
 #define TELEM_ON 1
 #define TELEM_OFF 0
 
-#define PROTO_TELEM_UNSUPPORTED -1
-
 #define FREQFINE g_model.rfOptionValue1 // CC2500 freq. tune
-
-
-//extern struct Model Model;
-
-//extern volatile uint8_t g_initializing;
-
-struct Transmitter {
-    uint8_t dummy;
-};
-
-//extern struct Transmitter Transmitter;
-
-// #define NULL ((void*) 0)
 
 #define NUM_OUT_CHANNELS 7
 
-//MAX = +10000
-//MIN = -10000
-// #define CHAN_MULTIPLIER 100
 
 // For Multiplex M-Link & PPM
 #define CHAN_MAX_VALUE (1520L)
@@ -82,15 +53,6 @@ struct Transmitter {
 //#define CHAN_MAX_VALUE (+1520 * 7)
 //#define CHAN_MIN_VALUE (-1520 * 7) // Range of -1521 to +1520.
 
-//extern volatile int16_t Channels[16];
-
-/* Temporary definition until we have real translation */
-#define _tr_noop(x) x
-#ifdef NO_LANGUAGE_SUPPORT
-#define _tr(x) x
-#else
-const char *_tr(const char *str);
-#endif
 
 
 #define COUNTS_PER_MILLI_SEC (F_CPU/128000L)-1 // value should == 1ms based on F_CPU.
