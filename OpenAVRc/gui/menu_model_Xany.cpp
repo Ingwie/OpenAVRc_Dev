868bcc59879ce827723a19fb5db489cc75a8c215
Bracame <Ingwie@users.noreply.github.com>
2018-06-18 19:12:06 +0200

    X_any menu model update

--- OpenAVRc/gui/menu_model_Xany.cpp
+++ OpenAVRc/gui/menu_model_Xany.cpp
@@ -43,6 +43,7 @@ enum menuModelXanyItems
   ITEM_MODEL_REPEATNB_A,
   ITEM_MODEL_ABSAGLSENSOR_A,
   ITEM_MODEL_INPUT_A,
+#if (X_ANY >= 2)
   ITEM_MODEL_SEPARATOR,
   ITEM_MODEL_NAME_B,
   ITEM_MODEL_ACTIVE_B,
@@ -50,6 +51,25 @@ enum menuModelXanyItems
   ITEM_MODEL_REPEATNB_B,
   ITEM_MODEL_ABSAGLSENSOR_B,
   ITEM_MODEL_INPUT_B,
+#endif
+#if (X_ANY >= 3)
+  ITEM_MODEL_SEPARATOR1,
+  ITEM_MODEL_NAME_C,
+  ITEM_MODEL_ACTIVE_C,
+  ITEM_MODEL_CHID_C,
+  ITEM_MODEL_REPEATNB_C,
+  ITEM_MODEL_ABSAGLSENSOR_C,
+  ITEM_MODEL_INPUT_C,
+#endif
+#if (X_ANY >= 4)
+  ITEM_MODEL_SEPARATOR2,
+  ITEM_MODEL_NAME_D,
+  ITEM_MODEL_ACTIVE_D,
+  ITEM_MODEL_CHID_D,
+  ITEM_MODEL_REPEATNB_D,
+  ITEM_MODEL_ABSAGLSENSOR_D,
+  ITEM_MODEL_INPUT_D,
+#endif
 };
 
 void menuModelXany(uint8_t event)
@@ -103,9 +123,16 @@ void menuModelXany(uint8_t event)
         case ITEM_MODEL_INPUT_A :
           ValidMsg = Xany_operation(0, XANY_OP_READ_INFO, &XanyInfo);
           /* If ValidMsg == 0, this means the combination is not supported */
-          lcd_outbin(XanyInfo.SwNb,4*FW,y,XanyInfo.SwValue,attr);
+          if (ValidMsg)
+            {
+              lcd_outbin(XanyInfo.SwNb,4*FW,y,XanyInfo.SwValue,attr);
+            }
+          else
+            {
+              lcdDrawTextAtt(0, y, STR_INCOMPATIBLE, attr ? attr : BLINK);
+            }
           break;
-
+#if (X_ANY >= 2)
         case ITEM_MODEL_SEPARATOR :
           break;
 
@@ -136,8 +163,98 @@ void menuModelXany(uint8_t event)
         case ITEM_MODEL_INPUT_B :
           ValidMsg = Xany_operation(1, XANY_OP_READ_INFO, &XanyInfo);
           /* If ValidMsg == 0, this means the combination is not supported */
-          lcd_outbin(XanyInfo.SwNb,4*FW,y,XanyInfo.SwValue,attr);
+          if (ValidMsg)
+            {
+              lcd_outbin(XanyInfo.SwNb,4*FW,y,XanyInfo.SwValue,attr);
+            }
+          else
+            {
+              lcdDrawTextAtt(0, y, STR_INCOMPATIBLE, attr ? attr : BLINK);
+            }
           break;
+#endif
+#if (X_ANY >= 3)
+        case ITEM_MODEL_SEPARATOR1 :
+          break;
+
+        case ITEM_MODEL_NAME_C :
+          lcdDrawStringWithIndex(0, y, STR_NUMBER, 3, attr);
+          break;
+
+        case ITEM_MODEL_ACTIVE_C :
+          ON_OFF_MENU_ITEM(g_model.Xany[2].Active, MODEL_XANY_2ND_COLUMN, y, STR_ACTIVED, attr, event);
+          break;
+
+        case ITEM_MODEL_CHID_C :
+          lcdDrawStringWithIndex(0, y, STR_CHANNEL, g_model.Xany[2].ChId+1, attr);
+          if (attr)
+            CHECK_INCDEC_MODELVAR_ZERO(event, g_model.Xany[2].ChId, NUM_CHNOUT);
+          break;
+
+        case ITEM_MODEL_REPEATNB_C :
+          lcdDrawStringWithIndex(0, y, STR_NB_REPEAT, g_model.Xany[2].RepeatNb, attr);
+          if (attr)
+            CHECK_INCDEC_MODELVAR_ZERO(event, g_model.Xany[2].RepeatNb, 3);
+          break;
+
+        case ITEM_MODEL_ABSAGLSENSOR_C :
+          ON_OFF_MENU_ITEM(g_model.Xany[2].PayloadCfg.Item.AbsAngleSensor, MODEL_XANY_2ND_COLUMN, y, STR_ANGLE_SENSOR, attr, event);
+          break;
+
+        case ITEM_MODEL_INPUT_C :
+          ValidMsg = Xany_operation(2, XANY_OP_READ_INFO, &XanyInfo);
+          /* If ValidMsg == 0, this means the combination is not supported */
+          if (ValidMsg)
+            {
+              lcd_outbin(XanyInfo.SwNb,4*FW,y,XanyInfo.SwValue,attr);
+            }
+          else
+            {
+              lcdDrawTextAtt(0, y, STR_INCOMPATIBLE, attr ? attr : BLINK);
+            }
+          break;
+#endif
+#if (X_ANY >= 4)
+        case ITEM_MODEL_SEPARATOR2 :
+          break;
+
+        case ITEM_MODEL_NAME_D :
+          lcdDrawStringWithIndex(0, y, STR_NUMBER, 4, attr);
+          break;
+
+        case ITEM_MODEL_ACTIVE_D :
+          ON_OFF_MENU_ITEM(g_model.Xany[3].Active, MODEL_XANY_2ND_COLUMN, y, STR_ACTIVED, attr, event);
+          break;
+
+        case ITEM_MODEL_CHID_D :
+          lcdDrawStringWithIndex(0, y, STR_CHANNEL, g_model.Xany[3].ChId+1, attr);
+          if (attr)
+            CHECK_INCDEC_MODELVAR_ZERO(event, g_model.Xany[3].ChId, NUM_CHNOUT);
+          break;
+
+        case ITEM_MODEL_REPEATNB_D :
+          lcdDrawStringWithIndex(0, y, STR_NB_REPEAT, g_model.Xany[3].RepeatNb, attr);
+          if (attr)
+            CHECK_INCDEC_MODELVAR_ZERO(event, g_model.Xany[3].RepeatNb, 3);
+          break;
+
+        case ITEM_MODEL_ABSAGLSENSOR_D :
+          ON_OFF_MENU_ITEM(g_model.Xany[3].PayloadCfg.Item.AbsAngleSensor, MODEL_XANY_2ND_COLUMN, y, STR_ANGLE_SENSOR, attr, event);
+          break;
+
+        case ITEM_MODEL_INPUT_D :
+          ValidMsg = Xany_operation(3, XANY_OP_READ_INFO, &XanyInfo);
+          /* If ValidMsg == 0, this means the combination is not supported */
+          if (ValidMsg)
+            {
+              lcd_outbin(XanyInfo.SwNb,4*FW,y,XanyInfo.SwValue,attr);
+            }
+          else
+            {
+              lcdDrawTextAtt(0, y, STR_INCOMPATIBLE, attr ? attr : BLINK);
+            }
+          break;
+#endif
         }
       y += FH;
     }
