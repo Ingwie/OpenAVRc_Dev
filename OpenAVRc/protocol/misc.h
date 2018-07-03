3e45030563c2c9ddb8cedf1d25b20a1ed8110ce8
Bracame <Ingwie@users.noreply.github.com>
2018-07-03 18:32:31 +0200

    Bad define in /protocol/misc.h

--- OpenAVRc/protocol/misc.h
+++ OpenAVRc/protocol/misc.h
@@ -50,20 +50,20 @@
 
 #define RXNUM           g_model.modelId
 
-#define RX_TX_ADDR_OFFSET       PULSES_BYTE_SIZE - 5
+#define RF_ID_ADDR_OFFSET_VAR   4
+#define RX_TX_ADDR_OFFSET       (PULSES_BYTE_SIZE - RF_ID_ADDR_OFFSET_VAR)
 #define MAX_PACKET              40
 #define CHANNEL_USED_OFFSET     MAX_PACKET
 #define MAX_CHANNEL             50
-#define FREE_BYTE_OFFSET        MAX_PACKET + MAX_CHANNEL
+#define FREE_BYTE_OFFSET        (MAX_PACKET + MAX_CHANNEL)
+#define PULSES_WORD_OFFSET_VAR  (FREE_BYTE_OFFSET / 2) // 45 (90 bytes)
+#define PULSES_BYTE_OFFSET_VAR  RX_TX_ADDR_OFFSET      // 140
 
-#define RF_ID_ADDR_OFFSET_VAR   4
-#define PULSES_WORD_OFFSET_VAR  FREE_BYTE_OFFSET/2 // 45 (90 bytes)
-#define PULSES_BYTE_OFFSET_VAR         PULSES_BYTE_SIZE - RF_ID_ADDR_OFFSET_VAR //139 - 143
 
 #if defined(SPIMODULES)
   uint8_t * packet = pulses2MHz.pbyte; //protocol global packet (Use 40 MAX)
   uint8_t * channel_used = &pulses2MHz.pbyte[CHANNEL_USED_OFFSET]; //protocol global channel (Use 50 MAX -> 54 bytes free to use in SPI protocols)
-  uint8_t * t_rf_id_addr = &pulses2MHz.pbyte[RX_TX_ADDR_OFFSET];
+  uint8_t * temp_rfid_addr = &pulses2MHz.pbyte[RX_TX_ADDR_OFFSET];
   #if defined(PROTO_HAS_CC2500)
     uint8_t calData[48];
     uint8_t * telem_save_data = &pulses2MHz.pbyte[PULSES_BYTE_OFFSET_VAR-40]; // [9] used in FrskyX telemetry
@@ -75,6 +75,7 @@
   uint16_t pword[PULSES_WORD_SIZE];  // 72
   uint8_t  pbyte[PULSES_BYTE_SIZE];  // 144
  */
+
 //U8
 #define rfState8           pulses2MHz.pbyte[PULSES_BYTE_OFFSET_VAR-1]
 #define channel_index      pulses2MHz.pbyte[PULSES_BYTE_OFFSET_VAR-2]
@@ -91,15 +92,16 @@
 #define start_tx_rx        pulses2MHz.pbyte[PULSES_BYTE_OFFSET_VAR-13]
 
 //U16
-#define packet_period pulses2MHz.pword[PULSES_WORD_OFFSET_VAR]
-#define bind_counter  pulses2MHz.pword[PULSES_WORD_OFFSET_VAR+1]
-#define rfState16     pulses2MHz.pword[PULSES_WORD_OFFSET_VAR+2]
+#define rfState16          pulses2MHz.pword[PULSES_WORD_OFFSET_VAR]
+#define bind_counter       pulses2MHz.pword[PULSES_WORD_OFFSET_VAR+1]
 
 
-extern void PROTO_Start_Callback(uint16_t us, uint16_t (*cb)());
-extern void PROTO_Stop_Callback();
-extern uint32_t CLOCK_getms();
-extern void CLOCK_delayms(uint32_t delay_ms);
-extern void PROTOCOL_SetBindState(tmr10ms_t t10ms);
+void PROTO_Start_Callback(uint16_t us, uint16_t (*cb)());
+void PROTO_Stop_Callback();
+uint32_t CLOCK_getms();
+void CLOCK_delayms(uint32_t delay_ms);
+void PROTOCOL_SetBindState(tmr10ms_t t10ms);
+void loadrfidaddr();
+void loadrfidaddr_rxnum(uint8_t addrnum);
 
 #endif
