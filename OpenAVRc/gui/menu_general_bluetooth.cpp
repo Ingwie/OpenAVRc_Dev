617bf35aaf75131020f66a281e2b3b323392101e
Bracame <Ingwie@users.noreply.github.com>
2020-01-19 22:30:55 +0100

    BT menu : unuse code

--- OpenAVRc/gui/menu_general_bluetooth.cpp
+++ OpenAVRc/gui/menu_general_bluetooth.cpp
@@ -132,7 +132,7 @@ void onPairSelected(const char *result)
 {
   // result is the new pair name!!
   strcpy(reusableBuffer.bluetooth.peer_name_str, result);
-  str2zchar(reusableBuffer.bluetooth.peer_name_str, reusableBuffer.bluetooth.peer_name_str, sizeof(reusableBuffer.bluetooth.peer_name_str));
+  //str2zchar(reusableBuffer.bluetooth.name_zchar, reusableBuffer.bluetooth.peer_name_str, sizeof(reusableBuffer.bluetooth.peer_name_str));
   memcpy(g_eeGeneral.BT.Peer.Mac, reusableBuffer.bluetooth.scann.Remote[shared_u8].MAC, BT_MAC_BIN_LEN);
   bluetooth_AtCmdMode(ON);
   IF_NO_ERROR(bluetooth_linkToRemote(g_eeGeneral.BT.Peer.Mac, BT_SET_TIMEOUT_MS))
