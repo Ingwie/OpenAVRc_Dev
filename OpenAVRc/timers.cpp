d6fb49c930e4ed1e8d7a255822d1cd8e01dcef5a
Bracame <Ingwie@users.noreply.github.com>
2020-02-21 21:07:59 +0100

    Timer does not decrease in trottle mode if gaz security is ON
    Thanks to Guilm on Modelisme.com

--- OpenAVRc/timers.cpp
+++ OpenAVRc/timers.cpp
@@ -75,6 +75,11 @@ void saveTimers()
 
 void evalTimers(int16_t throttle, uint8_t tick10ms)
 {
+  if (!enableGaz)
+  {
+    throttle = 0;
+  }
+
   for (uint8_t i=0; i<TIMERS; i++) {
     int8_t timerMode = g_model.timers[i].mode;
     tmrstart_t timerStart = g_model.timers[i].start;
