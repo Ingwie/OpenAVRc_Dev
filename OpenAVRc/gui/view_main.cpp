ad075adbafa4c317e5107a31da7b6a3bca4de154
Bracame <Ingwie@users.noreply.github.com>
2020-02-24 22:48:25 +0100

    Remove unused code

--- OpenAVRc/gui/view_main.cpp
+++ OpenAVRc/gui/view_main.cpp
@@ -502,13 +502,8 @@ void menuMainView(uint8_t event)
       for (uint8_t i=0; i<NUM_ROTARY_ENCODERS; i++) {
         int16_t val = getRotaryEncoder(i);
         int8_t len = limit((int16_t)0, (int16_t)(((val+1024) * BAR_HEIGHT) / 2048), (int16_t)BAR_HEIGHT);
-#if ROTARY_ENCODERS > 2
-#define V_BAR_W 5
-        V_BAR(LCD_W/2-8+V_BAR_W*i, LCD_H-8, len);
-#else
 #define V_BAR_W 5
         V_BAR(LCD_W/2-3+V_BAR_W*i, LCD_H-8, len);
-#endif
       }
 #endif // ROTARY_ENCODERS
 
