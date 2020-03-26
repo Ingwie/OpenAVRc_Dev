7989caa526567cf27886c550a4ad192dafec4207
Bracame <Ingwie@users.noreply.github.com>
2020-03-26 17:53:39 +0100

    remove unused code

--- OpenAVRc/targets/lcd/lcd_simu_driver.cpp
+++ OpenAVRc/targets/lcd/lcd_simu_driver.cpp
@@ -83,12 +83,7 @@ void lcdSetRefVolt(uint8_t val)
 
 void lcdRefreshFast()
 {
-  SHOWDURATIONLCD1
-#if defined(SHOWDURATION)
-  lcdDrawNumberAttUnit(16*FW, 1, DURATION_MS_PREC2(DurationValue), PREC2);
-#endif
   wxGetApp().SimuFrame->DrawWxSimuLcd();
-  SHOWDURATIONLCD2
 }
 
 void lcdRefresh()
