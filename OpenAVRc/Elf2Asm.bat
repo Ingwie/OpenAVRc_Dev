ea732045efb0cfe58b776da04b2b72ad83fb7da1
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-04-03 23:37:11 +0200

    test

--- OpenAVRc/Elf2Asm.bat
+++
@@ -1,8 +0,0 @@
-Path C:\OpenAVRc\AVR-GCC\bin;C:\OpenAVRc\AVR-GCC\utils\bin
-
-avr-objdump.exe -S  OpenAVRc.elf > OpenAVRc.S
-
-pause
-
-
-
--- OpenAVRc/Rebuild.bat
+++
@@ -1,5 +0,0 @@
-
-Path C:\OpenAVRc_Builder\AVR-GCC\bin;C:\OpenAVRc_Builder\AVR-GCC\utils\bin;C:\OpenAVRc_Builder\Python34
-make clean
-make PCB=MEGAMINI
-pause
