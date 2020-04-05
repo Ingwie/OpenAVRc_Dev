71214810d398c17a7692dd6e4fe4718f8109dba3
Bracame <Ingwie@users.noreply.github.com>
2020-04-06 01:26:50 +0200

    FW : Optimise Menu VERSION

--- OpenAVRc/gui/menu_general_version.cpp
+++ OpenAVRc/gui/menu_general_version.cpp
@@ -39,36 +39,21 @@ void menuGeneralVersion(uint8_t event)
 {
 #define VERS_ITEMS_COUNT 2
 
-  MENU(STR_MENUVERSION, menuTabGeneral, e_Vers, VERS_ITEMS_COUNT, {0, 3});
+ MENU(STR_MENUVERSION, menuTabGeneral, e_Vers, VERS_ITEMS_COUNT, {0, 3});
 
-  uint8_t sub = menuVerticalPosition - 1;
-  uint8_t blink = ((s_editMode>0) ? BLINK|INVERS : INVERS);
-  uint8_t attr = (sub == menuVerticalOffset ? blink : 0);
+ uint8_t sub = menuVerticalPosition - 1;
+ uint8_t blink = ((s_editMode>0) ? BLINK|INVERS : INVERS);
+ uint8_t attr = (sub == menuVerticalOffset ? blink : 0);
 
-  lcdDrawTextLeft(MENU_HEADER_HEIGHT+FH, vers_stamp);
+ lcdDrawTextLeft(MENU_HEADER_HEIGHT+FH, vers_stamp);
 
 #define ID_OFFSET 4 + 3*FW
 
-  lcdDrawTextLeft(7*FH, STR_RFID);
-  for (uint8_t j=0; j<4; j++) {
-    uint8_t rowattr = (menuHorizontalPosition==j ? attr : 0);
-    switch (j) {
-    case 0:
-      lcd_outhex(2, ID_OFFSET + 0*FW, 7*FH, g_eeGeneral.fixed_ID.ID_8[3], rowattr);
-      if (rowattr && (s_editMode>0 || p1valdiff)) CHECK_INCDEC_GENVAR(event, g_eeGeneral.fixed_ID.ID_8[3], 0, 0xFF);
-      break;
-    case 1:
-      lcd_outhex(2, ID_OFFSET + 2*FW, 7*FH, g_eeGeneral.fixed_ID.ID_8[2], rowattr);
-      if (rowattr && (s_editMode>0 || p1valdiff)) CHECK_INCDEC_GENVAR(event, g_eeGeneral.fixed_ID.ID_8[2], 0, 0xFF);
-      break;
-    case 2:
-      lcd_outhex(2, ID_OFFSET + 4*FW, 7*FH, g_eeGeneral.fixed_ID.ID_8[1], rowattr);
-      if (rowattr && (s_editMode>0 || p1valdiff)) CHECK_INCDEC_GENVAR(event, g_eeGeneral.fixed_ID.ID_8[1], 0, 0xFF);
-      break;
-    case 3:
-      lcd_outhex(2, ID_OFFSET + 6*FW, 7*FH, g_eeGeneral.fixed_ID.ID_8[0], rowattr);
-      if (rowattr && (s_editMode>0 || p1valdiff)) CHECK_INCDEC_GENVAR(event, g_eeGeneral.fixed_ID.ID_8[0], 0, 0xFF);
-      break;
-    }
+ lcdDrawTextLeft(7*FH, STR_RFID);
+ for (uint8_t j=0; j<4; j++)
+  {
+   uint8_t rowattr = (menuHorizontalPosition==j ? attr : 0);
+   lcd_outhex(2, ID_OFFSET + j*2*FW, 7*FH, g_eeGeneral.fixed_ID.ID_8[3-j], rowattr);
+   if (rowattr && (s_editMode>0 || p1valdiff)) CHECK_INCDEC_GENVAR(event, g_eeGeneral.fixed_ID.ID_8[3-j], 0, 0xFF);
   }
 }
