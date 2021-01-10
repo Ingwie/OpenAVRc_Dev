4ccdf73c3b1e17a416adb4d5a7cb0bc7e3cf32cc
pierrotm777 <pierrotm777@gmail.com>
2021-01-10 20:06:51 +0100

    Update BtToSimulator_PPM.ino
    Adapt code for a Pro Micro
    Change HC05 baud to 115200

--- PCB/Bluetooth/BtToSimulator/BtToSimulator_PPM/BtToSimulator_PPM.ino
+++ PCB/Bluetooth/BtToSimulator/BtToSimulator_PPM/BtToSimulator_PPM.ino
@@ -30,18 +30,11 @@
  **************************************************************************
 */
 
-#define DEBUG
+//#define DEBUG
 //#define CMD_MODE
 
 /*
- How to connect HC05/Pro Mini 8MHz/3,3v:
- Pro Mini     HC05
-    RAW(+5V)   Vcc
-    GND        GND
-    D8         Tx
-    D9         Rx
-    D11(PPM)
-How to connect HC05/Pro Mini 16MHz/5v:
+How to connect HC05/Pro Micro 16MHz/5v:
  Pro Mini     HC05
     VCC(+5V)   Vcc
     GND        GND
@@ -50,7 +43,7 @@ How to connect HC05/Pro Mini 16MHz/5v:
             2,2K
              |
             GND
-    D11(PPM)
+    D3(PPM)
  
  How to configure HC05:
  - Uncomment #define CMD_MODE in line 34
@@ -77,11 +70,7 @@ How to connect HC05/Pro Mini 16MHz/5v:
 #define CPPM_PERIOD_US        22500
 #include <TinyCppmGen.h>
 
-//#include <SoftwareSerial.h>
-#include <AltSoftSerial.h> //Header File for Serial Bluetooth, will be added by default into Arduino
-
-//SoftwareSerial BT(8,9);// Rx,Tx
-AltSoftSerial BT;//Rx=8,Tx=9 for all atmega328
+HardwareSerial &BT = Serial1;
 
 String str = "", ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8, CS;
 uint16_t n1, n2, n3, n4, n5, n6, n7, n8, Checksum;
@@ -93,10 +82,12 @@ static char PrintBuf[PRINT_BUF_SIZE + 1];
 #define PRINT_P(FlashStr)   do{if(Serial){Serial.print(F(FlashStr));}}while(0)
 #define PRINT_W(FlashStr)   do{if(Serial){Serial.print(FlashStr);}}while(0)
 
+#define BIN_NBL_TO_HEX_DIGIT(BinNbl)      ((BinNbl) < 10) ? ((BinNbl) + '0'): ((BinNbl) - 10 + 'A')
+
 void setup() {
 //  pinMode(PIN_MODE,INPUT_PULLUP);
   /*Serial BAUD doit etre moins rapide que celui du BT !!! */
-  Serial.begin(38400); //Start Serial monitor in 38400 maxi
+  Serial.begin(115200); //Start Serial monitor in 38400 maxi
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
@@ -105,17 +96,25 @@ void setup() {
     BT.begin(38400);
     PRINT_P("Start AT commands mode:\r\n\r\n");
 #else
-    BT.begin(57600);
+    BT.begin(115200);
     PRINT_P("Waiting data from OpenAVRc transmitter\r\n\r\n");
 #endif
   
 
-// PPM output pin is imposed by hardware and is target dependant:
-//(The user has to define Timer and Channel to use in TinyPpmGen.h file of the library)
-// TIMER(2), CHANNEL(A) -> OC2A -> PB3 -> Pin#11 here is used as output
-
+/* PPM output pin is imposed by hardware and is target dependant:
+   (The user has to define Timer and Channel to use in TinyPpmGen.h file of the library)
+       - ATmega328P (Arduino UNO):
+         TIMER(0), CHANNEL(A) -> OC0A -> PD6 -> Pin#6
+         TIMER(0), CHANNEL(B) -> OC0B -> PD5 -> Pin#5
+         TIMER(2), CHANNEL(A) -> OC2A -> PB3 -> Pin#11
+         TIMER(2), CHANNEL(B) -> OC2B -> PD3 -> Pin#3
+         
+       - ATmega32U4 (Arduino Leonardo, Micro and Pro Micro):
+         TIMER(0), CHANNEL(A) -> OC0A -> PB7 -> Pin#11 (/!\ pin not available on connector of Pro Micro /!\)
+         TIMER(0), CHANNEL(B) -> OC0B -> PD0 -> Pin#3
+*/
   //TinyCppmGen.begin(TINY_CPPM_GEN_POS_MOD, 8);
-  TinyCppmGen.begin(TINY_CPPM_GEN_NEG_MOD, CH_MAX_NB, CPPM_PERIOD_US);//Futaba use negative pulse
+  TinyCppmGen.begin(TINY_CPPM_GEN_POS_MOD, CH_MAX_NB, CPPM_PERIOD_US);//Futaba use negative pulse
 #ifdef DEBUG  
   PRINT_P("CH1\tCH2\tCH3\tCH4\tCH5\tCH6\tCH7\tCH8\tChecksum\r\n");
 #endif
@@ -136,9 +135,20 @@ void loop()
       {
         //tf command format:  sCh1sCh2sCh3sCh4sCh5sCh6sCh7sCh8:CS
   //      tf sHHHsHHHsHHHsHHHsHHHsHHHsHHHsHHH:CS<CR>   (CS=Checksum) -> Ex: tf -100+200-300+400-500+600-700+800:00
-        char character = BT.read();
-        str.concat(character);
-        if (character == '\r'){         
+        // voir http://grimaldi.univ-tln.fr/communiquer-en-bluetooth-avec-un-systeme-arduino-sous-qt.html
+        char character = (char)BT.read();
+        if (isHexadecimalDigit(character) ||character == 't' || character == 'f' ||character == ' ' || character == 's' || character == ':'|| character == '\r'|| character == '\n')
+        {
+          str.concat(character);
+        }
+        else
+        {
+          character = '\0';
+        }
+        if (character == '\n'){
+            str.replace("\r\n","\0");
+//          if (str.length() <= 38)
+//          {                    
             ch1 = getValue(str, 's', 1);n1 = hexToDec(ch1);
             ch2 = getValue(str, 's', 2);n2 = hexToDec(ch2);
             ch3 = getValue(str, 's', 3);n3 = hexToDec(ch3);
@@ -149,35 +159,56 @@ void loop()
             
             ch8 = getValue(str, 's', 8);
             CS = ch8;
-            ch8 = getValue(ch8, ':', 1);
+            ch8 = getValue(ch8, ':', 0);
             n8 = hexToDec(ch8);
-            CS  = getValue(CS , ':', 2);
+            CS  = getValue(str , ':', 1);
             Checksum = hexToDec(CS);
 
 #ifdef DEBUG
 //            PRINT_W(str);PRINT_P("\r\n");//see https://www.cplusplus.com/reference/cstdio/printf/
-            PRINTF("%u",n1);
-            PRINTF("\t%u",n2);
-            PRINTF("\t%u",n3);
-            PRINTF("\t%u",n4);
-            PRINTF("\t%u",n5);
-            PRINTF("\t%u",n6);
-            PRINTF("\t%u",n7);
-            PRINTF("\t%u",n8);
-            PRINTF("\t%u\r\n",Checksum);             
+              Serial.print(n1);
+              Serial.print("\t");Serial.print(n2);
+              Serial.print("\t");Serial.print(n3);
+              Serial.print("\t");Serial.print(n4);
+              Serial.print("\t");Serial.print(n5);
+              Serial.print("\t");Serial.print(n6);
+              Serial.print("\t");Serial.print(n7);
+              Serial.print("\t");Serial.print(n8);
+              Serial.print("\t");Serial.println(Checksum);
+//            PRINTF("%u",n1);
+//            PRINTF("\t%u",n2);
+//            PRINTF("\t%u",n3);
+//            PRINTF("\t%u",n4);
+//            PRINTF("\t%u",n5);
+//            PRINTF("\t%u",n6);
+//            PRINTF("\t%u",n7);
+//            PRINTF("\t%u",n8);
+//            PRINTF("\t%x\r\n",CS);             
 #endif
-            str = ""; 
-        //  Mod1 Dir Prof Gaz Ail
-            TinyCppmGen.setChWidth_us(1, n1); //OpenAVRc Trottle
-            TinyCppmGen.setChWidth_us(2, n2);
-            TinyCppmGen.setChWidth_us(3, n3); 
-            TinyCppmGen.setChWidth_us(4, n4);
-            TinyCppmGen.setChWidth_us(5, n5); 
-            TinyCppmGen.setChWidth_us(6, n6); 
-            TinyCppmGen.setChWidth_us(7, n7); 
-            TinyCppmGen.setChWidth_us(8, n8);          
+            str = "";
+//          } 
+       
         }
       }
+      Serial.print(n1);
+      Serial.print("\t");Serial.print(n2);
+      Serial.print("\t");Serial.print(n3);
+      Serial.print("\t");Serial.print(n4);
+      Serial.print("\t");Serial.print(n5);
+      Serial.print("\t");Serial.print(n6);
+      Serial.print("\t");Serial.print(n7);
+      Serial.print("\t");Serial.print(n8);
+      Serial.print("\t");Serial.println(Checksum);      
+      //  Mod1 Dir Prof Gaz Ail
+      TinyCppmGen.setChWidth_us(1, n1); //OpenAVRc Trottle
+      TinyCppmGen.setChWidth_us(2, n2);
+      TinyCppmGen.setChWidth_us(3, n3); 
+      TinyCppmGen.setChWidth_us(4, n4);
+      TinyCppmGen.setChWidth_us(5, n5); 
+      TinyCppmGen.setChWidth_us(6, n6); 
+      TinyCppmGen.setChWidth_us(7, n7); 
+      TinyCppmGen.setChWidth_us(8, n8);   
+      
 #endif
 }//END LOOP
 
@@ -200,7 +231,7 @@ String getValue(String data, char separator, int index)
 }
 
 //https://github.com/benrugg/Arduino-Hex-Decimal-Conversion/blob/master/hex_dec.ino
-unsigned int hexToDec(String hexString) {
+uint16_t hexToDec(String hexString) {
   
   unsigned int decValue = 0;
   int nextInt;
