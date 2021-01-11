82cb879fe5606367a2e437f1b82c19186b0af9dd
pierrotm777 <pierrotm777@gmail.com>
2021-01-11 19:39:55 +0100

    Update BtToSimulator_PPM.ino
    Change Serial read code

--- PCB/Bluetooth/BtToSimulator/BtToSimulator_PPM/BtToSimulator_PPM.ino
+++ PCB/Bluetooth/BtToSimulator/BtToSimulator_PPM/BtToSimulator_PPM.ino
@@ -30,8 +30,18 @@
  **************************************************************************
 */
 
-//#define DEBUG
+#define DEBUG
 //#define CMD_MODE
+
+#define LONGUEUR_MSG_MAX   50             /* ex: tf sHHHsHHHsHHHsHHHsHHHsHHHsHHHsHHH:CS<CR> */
+#define RETOUR_CHARRIOT    0x0D           /* CR (code ASCII) */
+#define PASSAGE_LIGNE      0x0A           /* LF (code ASCII) */
+#define BACK_SPACE         0x08
+char Message[LONGUEUR_MSG_MAX + 1];
+uint8_t SubStrNb, SeparFound;
+#define SUB_STRING_NB_MAX  8              /* nombre de valeurs splitées */
+char *StrTbl[SUB_STRING_NB_MAX];          /* declaration de pointeurs sur chaine, 1 pointeur = 2 octets seulement */
+
 
 /*
 How to connect HC05/Pro Micro 16MHz/5v:
@@ -48,7 +58,7 @@ How to connect HC05/Pro Micro 16MHz/5v:
  How to configure HC05:
  - Uncomment #define CMD_MODE in line 34
  - Upload sketch
- - Define console in 38400 bauds
+ - Define console in 115200 bauds
  - Hold KEY button and Power on module
  - Type in console:
    AT (module return OK)
@@ -56,7 +66,7 @@ How to connect HC05/Pro Micro 16MHz/5v:
    AT+NAME=BT/SIM (or what you want, module return OK)
    AT+ROLE=0 (configure in slave mode and return OK)
    AT+PSWD="1234" (use same password in your TX, return OK)
-   AT+UART=57600,0,0
+   AT+UART=115200,0,0
    
  - Comment #define CMD_MODE in line 34
  - Upload sketch
@@ -66,14 +76,17 @@ How to connect HC05/Pro Micro 16MHz/5v:
 #include <Rcul.h>
 #include <TinyPinChange.h>
 
-#define CH_MAX_NB  8
-#define CPPM_PERIOD_US        22500
+#define CH_MAX_NB  4
+#define PULSE_WIDTH_MIN_US    1000
+#define PULSE_WIDTH_MAX_US    2000
+#define CPPM_PERIOD_US        20000
 #include <TinyCppmGen.h>
 
 HardwareSerial &BT = Serial1;
 
 String str = "", ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8, CS;
 uint16_t n1, n2, n3, n4, n5, n6, n7, n8, Checksum;
+uint16_t ppmOut[9];
 
 #define PRINT_BUF_SIZE          100
 static char PrintBuf[PRINT_BUF_SIZE + 1];
@@ -85,7 +98,6 @@ static char PrintBuf[PRINT_BUF_SIZE + 1];
 #define BIN_NBL_TO_HEX_DIGIT(BinNbl)      ((BinNbl) < 10) ? ((BinNbl) + '0'): ((BinNbl) - 10 + 'A')
 
 void setup() {
-//  pinMode(PIN_MODE,INPUT_PULLUP);
   /*Serial BAUD doit etre moins rapide que celui du BT !!! */
   Serial.begin(115200); //Start Serial monitor in 38400 maxi
   while (!Serial) {
@@ -113,8 +125,8 @@ void setup() {
          TIMER(0), CHANNEL(A) -> OC0A -> PB7 -> Pin#11 (/!\ pin not available on connector of Pro Micro /!\)
          TIMER(0), CHANNEL(B) -> OC0B -> PD0 -> Pin#3
 */
-  //TinyCppmGen.begin(TINY_CPPM_GEN_POS_MOD, 8);
-  TinyCppmGen.begin(TINY_CPPM_GEN_POS_MOD, CH_MAX_NB, CPPM_PERIOD_US);//Futaba use negative pulse
+//  TinyCppmGen.begin(TINY_CPPM_GEN_NEG_MOD, 8);
+  TinyCppmGen.begin(TINY_CPPM_GEN_NEG_MOD, CH_MAX_NB, CPPM_PERIOD_US);//Futaba use negative pulse
 #ifdef DEBUG  
   PRINT_P("CH1\tCH2\tCH3\tCH4\tCH5\tCH6\tCH7\tCH8\tChecksum\r\n");
 #endif
@@ -131,104 +143,30 @@ void loop()
     BT.write(Serial.read());
 #else
 
-      while(BT.available())
-      {
-        //tf command format:  sCh1sCh2sCh3sCh4sCh5sCh6sCh7sCh8:CS
-  //      tf sHHHsHHHsHHHsHHHsHHHsHHHsHHHsHHH:CS<CR>   (CS=Checksum) -> Ex: tf -100+200-300+400-500+600-700+800:00
-        // voir http://grimaldi.univ-tln.fr/communiquer-en-bluetooth-avec-un-systeme-arduino-sous-qt.html
-        char character = (char)BT.read();
-        if (isHexadecimalDigit(character) ||character == 't' || character == 'f' ||character == ' ' || character == 's' || character == ':'|| character == '\r'|| character == '\n')
-        {
-          str.concat(character);
-        }
-        else
-        {
-          character = '\0';
-        }
-        if (character == '\n'){
-            str.replace("\r\n","\0");
-//          if (str.length() <= 38)
-//          {                    
-            ch1 = getValue(str, 's', 1);n1 = hexToDec(ch1);
-            ch2 = getValue(str, 's', 2);n2 = hexToDec(ch2);
-            ch3 = getValue(str, 's', 3);n3 = hexToDec(ch3);
-            ch4 = getValue(str, 's', 4);n4 = hexToDec(ch4);
-            ch5 = getValue(str, 's', 5);n5 = hexToDec(ch5);
-            ch6 = getValue(str, 's', 6);n6 = hexToDec(ch6);
-            ch7 = getValue(str, 's', 7);n7 = hexToDec(ch7);
-            
-            ch8 = getValue(str, 's', 8);
-            CS = ch8;
-            ch8 = getValue(ch8, ':', 0);
-            n8 = hexToDec(ch8);
-            CS  = getValue(str , ':', 1);
-            Checksum = hexToDec(CS);
-
-#ifdef DEBUG
-//            PRINT_W(str);PRINT_P("\r\n");//see https://www.cplusplus.com/reference/cstdio/printf/
-              Serial.print(n1);
-              Serial.print("\t");Serial.print(n2);
-              Serial.print("\t");Serial.print(n3);
-              Serial.print("\t");Serial.print(n4);
-              Serial.print("\t");Serial.print(n5);
-              Serial.print("\t");Serial.print(n6);
-              Serial.print("\t");Serial.print(n7);
-              Serial.print("\t");Serial.print(n8);
-              Serial.print("\t");Serial.println(Checksum);
-//            PRINTF("%u",n1);
-//            PRINTF("\t%u",n2);
-//            PRINTF("\t%u",n3);
-//            PRINTF("\t%u",n4);
-//            PRINTF("\t%u",n5);
-//            PRINTF("\t%u",n6);
-//            PRINTF("\t%u",n7);
-//            PRINTF("\t%u",n8);
-//            PRINTF("\t%x\r\n",CS);             
-#endif
-            str = "";
-//          } 
-       
-        }
-      }
-      Serial.print(n1);
-      Serial.print("\t");Serial.print(n2);
-      Serial.print("\t");Serial.print(n3);
-      Serial.print("\t");Serial.print(n4);
-      Serial.print("\t");Serial.print(n5);
-      Serial.print("\t");Serial.print(n6);
-      Serial.print("\t");Serial.print(n7);
-      Serial.print("\t");Serial.print(n8);
-      Serial.print("\t");Serial.println(Checksum);      
+  ReadMessageFromBT();
+//#ifdef DEBUG
+//  PRINTF("%u",ppmOut[1]);
+//  PRINTF("\t%u",ppmOut[2]);
+//  PRINTF("\t%u",ppmOut[3]);
+//  PRINTF("\t%u",ppmOut[4]);
+//  PRINTF("\t%u",ppmOut[5]);
+//  PRINTF("\t%u",ppmOut[6]);
+//  PRINTF("\t%u",ppmOut[7]);
+//  PRINTF("\t%u",ppmOut[8]);
+//  PRINTF("\t%x\r\n",ppmOut[9]);
+//#endif    
       //  Mod1 Dir Prof Gaz Ail
-      TinyCppmGen.setChWidth_us(1, n1); //OpenAVRc Trottle
-      TinyCppmGen.setChWidth_us(2, n2);
-      TinyCppmGen.setChWidth_us(3, n3); 
-      TinyCppmGen.setChWidth_us(4, n4);
-      TinyCppmGen.setChWidth_us(5, n5); 
-      TinyCppmGen.setChWidth_us(6, n6); 
-      TinyCppmGen.setChWidth_us(7, n7); 
-      TinyCppmGen.setChWidth_us(8, n8);   
-      
-#endif
-}//END LOOP
-
-
-String getValue(String data, char separator, int index)
-{
-  int found = 0;
-  int strIndex[] = {0, -1};
-  int maxIndex = data.length()-1;
-
-  for(int i=0; i<=maxIndex && found<=index; i++){
-    if(data.charAt(i)==separator || i==maxIndex){
-        found++;
-        strIndex[0] = strIndex[1]+1;
-        strIndex[1] = (i == maxIndex) ? i+1 : i;
-    }
+  if (TinyCppmGen.isSynchro())
+  {
+    for (uint8_t i = 1; i < 9 ;i++)
+    {
+      TinyCppmGen.setChWidth_us(i, ppmOut[i]);
+    }  
   }
 
-  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
-}
+  delay(10);
+#endif
+}//END LOOP
 
 //https://github.com/benrugg/Arduino-Hex-Decimal-Conversion/blob/master/hex_dec.ino
 uint16_t hexToDec(String hexString) {
@@ -247,4 +185,135 @@ uint16_t hexToDec(String hexString) {
     decValue = (decValue * 16) + nextInt;
   }
     return decValue;
+}
+
+void ReadMessageFromBT() {
+  if (MsgDisponible() >= 0) 
+  {
+    String mystr = (String)Message;
+    mystr.replace("\r\n","\0");
+#ifdef DEBUG
+//    PRINT_W(mystr);PRINT_P("\r\n");
+#endif
+    if (strlen(Message) == 39)
+    {
+      static String checkMess;     
+      StrSplit(Message, "s",  StrTbl, SUB_STRING_NB_MAX, &SeparFound);
+      checkMess = StrTbl[0];
+//      PRINT_W(checkMess);PRINT_P("\r\n");
+      if (checkMess == "tf ")
+      {
+        for (uint8_t p = 1; p < 9 ; p++)
+        {
+         //ppmOut[p] = constrain(hexToDec(StrTbl[p]),PULSE_WIDTH_MIN_US,PULSE_WIDTH_MAX_US);
+         ppmOut[p] = hexToDec(StrTbl[p]);        
+        }
+        StrSplitRestore("s", StrTbl, SeparFound);//Imperatif SeparFound <= SUB_STRING_NB_MAX
+        StrSplit(Message, ":",  StrTbl, SUB_STRING_NB_MAX, &SeparFound);
+        ppmOut[9] = StrTbl[1];
+//#ifdef DEBUG
+//        PRINTF("%u",ppmOut[1]);
+//        PRINTF("\t%u",ppmOut[2]);
+//        PRINTF("\t%u",ppmOut[3]);
+//        PRINTF("\t%u",ppmOut[4]);
+//        PRINTF("\t%u",ppmOut[5]);
+//        PRINTF("\t%u",ppmOut[6]);
+//        PRINTF("\t%u",ppmOut[7]);
+//        PRINTF("\t%u",ppmOut[8]);
+//        PRINTF("\t%x\r\n",ppmOut[9]);
+//#endif 
+      }       
+    }
+  }
+}
+
+int8_t StrSplit(char *SrcStr, char *Separ,  char **TarStrTbl, uint8_t TblLenMax, uint8_t *SeparFound)/* merci a LOUSSOUARN Philippe pour ce code */
+{
+  uint8_t SubStrFound, SeparLen;
+  char *StartStr, *SeparPos;
+
+  *SeparFound = 0;
+  SubStrFound = 0;
+  /* Clear Table Entries (secu) in case of empty string */
+  for(uint8_t Idx = 0; Idx < TblLenMax; Idx++)
+  {
+    TarStrTbl[Idx] = NULL; /* End of string */
+  }
+  if (!strlen(SrcStr)) return(0);
+  StartStr = SrcStr; /* Do NOT touch SrcStr */
+  SeparLen = strlen(Separ);
+  while(TblLenMax)
+  {
+    SeparPos = strstr(StartStr, Separ);
+    if(SeparPos)
+    {
+      /* OK Substring found */
+      *SeparPos = 0;
+      TarStrTbl[(*SeparFound)++] = StartStr;
+      StartStr = SeparPos + SeparLen;
+      SubStrFound++; // <---- Correction d'un bug
+    }
+    else
+    {
+      SubStrFound = *SeparFound;
+      if(*StartStr)
+      {
+        TarStrTbl[*SeparFound] = StartStr;
+        SubStrFound++;
+      }
+      break;
+    }
+    TblLenMax--;
+  }
+  return(SubStrFound);
+}
+
+int8_t StrSplitRestore(char *Separ,  char **TarStrTbl, uint8_t SeparNbToRestore)/* merci a LOUSSOUARN Philippe pour ce code */
+{
+  uint8_t Idx, Restored = 0;
+  
+  for(Idx = 0; Idx < SeparNbToRestore; Idx++)
+  {
+    /* Restore \0 with first char of Separator */
+    if(TarStrTbl[Idx] != NULL)
+    {
+      TarStrTbl[Idx][strlen(TarStrTbl[Idx])] = Separ[0];
+      Restored++;
+    }
+  }
+  return(Restored);
+}
+
+int8_t MsgDisponible(void)/* merci a LOUSSOUARN Philippe pour ce code */
+{
+  int8_t Ret = -1;
+  char CaractereRecu;
+  static uint8_t Idx = 0;
+
+  
+  if(BT.available() > 0)
+  {
+    CaractereRecu = BT.read();
+    switch(CaractereRecu)
+    {
+      case RETOUR_CHARRIOT:
+      case PASSAGE_LIGNE:
+        Message[Idx] = 0;
+        Ret = Idx;
+        Idx = 0;
+        break; 
+      case BACK_SPACE: // Gestion touche d'effacement du dernier caractere sur un terminal fonctionnant caractere par caractere (ex: HyperTerminal, TeraTerm, etc...)
+        if(Idx) Idx--;
+        break;
+      default:
+        if(Idx < LONGUEUR_MSG_MAX)
+        {
+            Message[Idx] = CaractereRecu;
+            Idx++;
+        }
+        else Idx = 0; /* Reset index for the next message */
+        break;
+    }
+  }
+  return(Ret); 
 }
