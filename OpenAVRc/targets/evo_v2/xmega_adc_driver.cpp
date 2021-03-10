21a6f3924efe8f8fbf2324cc6b49a63ec9c46b2e
sloped-soarer <kilvi2013@gmail.com>
2021-03-10 14:29:54 +0000

    Clean up code and adjust TX_VOLTAGE measurment.

--- OpenAVRc/targets/evo_v2/xmega_adc_driver.cpp
+++ OpenAVRc/targets/evo_v2/xmega_adc_driver.cpp
@@ -18,6 +18,8 @@ inline void ADC_atomic(uint8_t Analogs, int16_t sample);
 #define PRODSIGNATURES_ADCACAL1  _SFR_MEM8(0x0021)
 #endif
 
+#define REF_SELECT  ADCA.REFCTRL = ADC_REFSEL_AREFA_gc | ADC_BANDGAP_bm
+
 #if 0
 typedef enum ADC_CURRLIMIT_enum
 {
@@ -48,9 +50,9 @@ void adcInit()
 
   ADCA.PRESCALER = ADC_PRESCALER_DIV32_gc; // DIV32 = 1MHz Clock (Max is 1.8 MHz).
 
-  ADCA.REFCTRL = ADC_BANDGAP_bm;
+  REF_SELECT;
 
-  ADCA_SAMPCTRL = 63; //0x3F; // SAMPCTRL = 0x3F = 63  Sampling time = (63+1)*(ClkADC/2).
+  ADCA_SAMPCTRL = 0x3F; // SAMPCTRL = 0x3F = 63  Sampling time = (63+1)*(ClkADC/2).
 
   ADCA.CH0.INTFLAGS |= ADC_CH_CHIF_bm; // Clear flag.
   ADCA.CH0.CTRL |= ADC_CH_START_bm; // Start Conversion.
@@ -99,28 +101,7 @@ void getADC()
         // VINN = AVCC_REF_N = 3.3V/2 connected to PINADC4.
         // Measured range = 3285 counts.
 
-/*
-        // File iox256d3.h is wrong. Use this typedef.
-        typedef enum ADC_CH_MUXNEG_enum
-        {
-            ADC_CH_MUXNEG_PIN4_gc = (0x00<<0),  // Input pin 4.
-            ADC_CH_MUXNEG_PIN5_gc = (0x01<<0),  // Input pin 5.
-            ADC_CH_MUXNEG_PIN6_gc = (0x02<<0),  // Input pin 6.
-            ADC_CH_MUXNEG_PIN7_gc = (0x03<<0),  // Input pin 7.
-        } ADC_CH_MUXNEG_t;
-
-        // Reference voltage selection.
-        typedef enum ADC_REFSEL_enum
-        {
-            ADC_REFSEL_INT1V_gc = (0x00<<4),  // Internal 1V.
-            ADC_REFSEL_INTVCC_gc = (0x01<<4),  // Internal VCC / 1.6.
-            ADC_REFSEL_AREFA_gc = (0x02<<4),  // External reference on PORT A.
-            ADC_REFSEL_AREFB_gc = (0x03<<4),  // External reference on PORT B.
-            ADC_REFSEL_INTVCC2_gc = (0x04<<4),  // Internal VCC / 2.
-        } ADC_REFSEL_t;
-*/
-
-        ADCA.REFCTRL = ADC_REFSEL_AREFA_gc | ADC_BANDGAP_bm; //
+        REF_SELECT;
 
         ADCA.CH0.MUXCTRL = 0b000 << ADC_CH_MUXNEG_gp; // PIN0
         ADCA.CH0.MUXCTRL |= adc_output << ADC_CH_MUXPOS_gp;
@@ -150,7 +131,7 @@ void getADC()
         // Differential measurement. VREF = AREFA (Anal' 3V3 /2).
         // VINN = PINADC ... also AREFA.
 
-        ADCA.REFCTRL = ADC_REFSEL_AREFA_gc | ADC_BANDGAP_bm;
+        REF_SELECT;
 
         ADCA.CH0.CTRL = ADC_CH_INPUTMODE_DIFF_gc; //
 
@@ -176,12 +157,11 @@ void getADC()
         // Differential measurement. VREF = 1.65V from AREF pin.
         // VINN = Pad GND.
 
-        ADCA.REFCTRL = ADC_REFSEL_AREFA_gc | ADC_BANDGAP_bm;
+        REF_SELECT;
 
         ADCA.CH0.CTRL = ADC_CH_INPUTMODE_DIFF_gc; //
 
-        ADCA.CH0.MUXCTRL = 0b101 << ADC_CH_MUXNEG_gp; // Pad GND
-        ADCA.CH0.MUXCTRL |= TX_VOLTAGE << ADC_CH_MUXPOS_gp;
+        ADCA.CH0.MUXCTRL = (TX_VOLTAGE << ADC_CH_MUXPOS_gp) | (0b111 << ADC_CH_MUXNEG_gp); // Internal GND.
 
         ADCA.CH0.INTFLAGS |= ADC_CH_CHIF_bm; // Clear flag.
         ADCA.CH0.CTRL |= ADC_CH_START_bm; // Start Conversion.
@@ -201,8 +181,8 @@ void getADC()
 
         // Differential measurement of ADC5 and ADC5 to test gain stage.
 
-        ADCA.REFCTRL = ADC_REFSEL_AREFA_gc | ADC_BANDGAP_bm; //
-
+        REF_SELECT;
+        
         ADCA.CH0.MUXCTRL = 0b001 << ADC_CH_MUXNEG_gp; // ADC5
         ADCA.CH0.MUXCTRL |= adc_output << ADC_CH_MUXPOS_gp;
         ADCA.CH0.CTRL = ADC_CH_INPUTMODE_DIFFWGAIN_gc | ADC_CH_GAIN_4X_gc; //
