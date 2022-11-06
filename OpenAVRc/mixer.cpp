9b9a1989f2414ee1d902fa26916f63524e71789a
RC-Navy <p.loussouarn@free.fr>
2022-11-06 14:15:49 +0100

    Mixer shall not overwrite ch used by Xany in PCM

--- OpenAVRc/mixer.cpp
+++ OpenAVRc/mixer.cpp
@@ -793,7 +793,11 @@ void evalMixes(uint8_t tick10ms)
     int16_t value = applyLimits(i, q);  // applyLimits will remove the 256 100% basis
       ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
       {
+#if (PCM_PROTOCOL == YES)
+        if(!(Proto.Pcm.XanyChMap & (1 << i))) channelOutputs[i] = value;  // copy consistent word to int-level
+#else
         channelOutputs[i] = value;  // copy consistent word to int-level
+#endif
       }
     }
 
