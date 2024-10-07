44ee8a3eff1cd4307804f3679712e2e73b729052
sloped-soarer <kilvi2013@gmail.com>
2024-10-07 20:08:38 +0100

    Update evo_lcd_driver.cpp
    Updated Evo LCD driver.

--- OpenAVRc/targets/evo_v2/evo_lcd_driver.cpp
+++ OpenAVRc/targets/evo_v2/evo_lcd_driver.cpp
@@ -16,12 +16,11 @@
  * the unused MISO pin.
  * USART MSPI would be better and can do DMA
  * too unlike the SPI port.
- * No Reset signal as that is derived from RTC chip.
+ * No Reset signal as that is derived from RTC chip / board.
  *****************************************************************
 */
 
 //#define BITBANGSPI
-
 #if !defined (BITBANGSPI)
 #include "spi.h"
 #endif
@@ -32,7 +31,7 @@
 #define ST7565R_CMD_DISPLAY_ALL_POINTS_OFF      0xA4
 #define ST7565R_CMD_REVERSE_SCAN_DIRECTION      0xC8
 #define ST7565R_CMD_ELECTRONIC_VOLUME_MODE_SET  0x81
-#define ST7565R_CMD_VOLTAGE_RESISTOR_RATIO_5    0x25
+#define ST7565R_CMD_VOLTAGE_RESISTOR_RATIO_5dec5    0x25
 #define ST7565R_CMD_POWER_CTRL_ALL_ON           0x2F
 #define ST7565R_CMD_DISPLAY_ON                  0xAF
 #define ST7565R_CMD_START_LINE_SET( line )             (0x40 | (line))
@@ -60,7 +59,7 @@
 
 #define O_LCD_CS           PIN4_bm
 
-//#define POSITIVE_CHIP_SELECT
+#define POSITIVE_CHIP_SELECT
 #if defined (POSITIVE_CHIP_SELECT)
 #define LCD_CS_ACTIVE      LCD_PORT.OUTSET = O_LCD_CS
 #define LCD_CS_INACTIVE    LCD_PORT.OUTCLR = O_LCD_CS
@@ -113,7 +112,7 @@ void lcdSendCtl(uint8_t c)
 void lcdSetRefVolt(uint8_t val)
 {
   lcdSendCtl(ST7565R_CMD_ELECTRONIC_VOLUME_MODE_SET); // Range = 0x09 to 0x19 (-8 to +8 on Evo setup screen)
-  lcdSendCtl(val - CONTRAST_MIN);
+  lcdSendCtl(val);
 }
 
 
@@ -152,25 +151,25 @@ void lcdInit()
 #endif
 
  _delay_us(300);
-
  lcdSendCtl(ST7565R_CMD_RESET);
  _delay_us(300);
+
  lcdSendCtl(0x2C);// Power controller set, booster cct on, V reg off, V follower off
  _delay_us(300);
  lcdSendCtl(0x2E);// Power controller set, booster cct on, V reg on, V follower off
  _delay_us(300);
  lcdSendCtl(ST7565R_CMD_POWER_CTRL_ALL_ON);// Power controller set, booster cct on, V reg on, V follower on
  _delay_us(300);
- lcdSendCtl(ST7565R_CMD_VOLTAGE_RESISTOR_RATIO_5);
+ lcdSendCtl(ST7565R_CMD_VOLTAGE_RESISTOR_RATIO_5dec5);
 
  lcdSendCtl(ST7565R_CMD_DISPLAY_OFF);
  // Turn display on in lcdRefresh() ... prevents displaying garbage at startup, which seems to happen more when sdcards are attached.
 
  lcdSendCtl(ST7565R_CMD_ELECTRONIC_VOLUME_MODE_SET);// Electronic volume mode set ? Range = 0x09 to 0x19 (-8 to +8 on Evo)
- lcdSendCtl(0x09);// See above ... 0 = 0x11
+ lcdSendCtl(CONTRAST_MIN);// See above ... 0 = 0x11
 
  lcdSendCtl(ST7565R_CMD_REVERSE_SCAN_DIRECTION);// Common output mode select
- lcdSendCtl(ST7565R_CMD_DISPLAY_ALL_POINTS_OFF);// All points off (normal)
+ //lcdSendCtl(ST7565R_CMD_DISPLAY_ALL_POINTS_OFF);// All points off (normal)
 }
 
 
