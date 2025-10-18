d4a27f203bb81ebcc632398bc962db1d26541691
Bracame <Ingwie@users.noreply.github.com>
2025-10-18 18:49:37 +0200

    FW : Fix compilation warning if no telemetry

--- OpenAVRc/Serial0.h
+++ OpenAVRc/Serial0.h
@@ -58,17 +58,16 @@
 #define IBUS_TLM_HEADER      9
 #define IBUS_TLM_PACKET_SIZE 37
 
+extern uint8_t Usart0TxBufferCount;
+
+///////////  Telemetry fields  ///////////////
+#if defined(FRSKY)
+
 static union
 {
   uint8_t TelemetryRxBuffer[NUM_TELEM_RX_BUFFER][FRSKY_TLM_PKT_SIZE];
   uint8_t ibus_telem_buffer[IBUS_TLM_PACKET_SIZE - IBUS_TLM_HEADER + 1]; // Omit TXID, RXID (8 bytes) but retain header byte 0xAA or 0xAC.
 };
-
-
-extern uint8_t Usart0TxBufferCount;
-
-///////////  Telemetry fields  ///////////////
-#if defined(FRSKY)
 
 void LoadTelemBuffer(uint8_t *data);
 void LoadAFHDS2ATelemBuffer(uint8_t *data);
