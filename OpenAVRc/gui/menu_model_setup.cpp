32ada7bc3b316a6be23233645d7bb3df9c39c8c7
Bracame <Ingwie@users.noreply.github.com>
2025-11-22 17:59:23 +0100

    Remove forcing condition on Multi version toggle ...
    Oups

--- OpenAVRc/gui/menu_model_setup.cpp
+++ OpenAVRc/gui/menu_model_setup.cpp
@@ -362,7 +362,7 @@ MENU_TAB({ 0, 0, 2, CASE_PERSISTENT_TIMERS(0) 0, 0, 2, CASE_PERSISTENT_TIMERS(0)
 #endif
       {
 #if (SERIAL_PROTOCOL==MULTIMODULE)
-        if ((IS_MULTIMODULE_PROTOCOL(protocol)) && (1) \
+        if ((IS_MULTIMODULE_PROTOCOL(protocol)) && (mm_type1_packet_ptr->input_signal_detected) \
             && (DOUBLE_BLINK_ON_PHASE)) // toggle xmitter-version if multimodule link is ok
           {
             //v1.3.4.31
