18eab3fe836bbcc888f3c6f00d4137cf595a3a87
Bracame <Ingwie@users.noreply.github.com>
2025-11-26 12:42:56 +0100

    check crsf p2m size

--- OpenAVRc/OpenAVRc.cpp
+++ OpenAVRc/OpenAVRc.cpp
@@ -1565,10 +1565,11 @@ int16_t simumain()
   simu_firstloop_is_runing = true;
   simu_off = false;
 
-  // check some size
-  /*uint32_t testsize_Pcm = sizeof(pulses2MHz.Pcm);testsize_Pcm/=1;
-  uint32_t testsize_mm_st = sizeof(pulses2MHz.mm_st);testsize_mm_st/=1;
-  uint32_t testsize_spi = sizeof(pulses2MHz.spi);testsize_spi/=1;*/
+  // check some size with simu break point
+  //uint32_t testsize_Pcm = sizeof(pulses2MHz.Pcm);testsize_Pcm/=1;
+  //uint32_t testsize_mm_st = sizeof(pulses2MHz.mm_st);testsize_mm_st/=1;
+  //uint32_t testsize_crsf_st = sizeof(pulses2MHz.crsf_st);testsize_crsf_st/=1;
+  //uint32_t testsize_spi = sizeof(pulses2MHz.spi);testsize_spi/=1;
 
 #endif
 
